b4bc657359d4c8cacb778ab84b91b9b985365bbc
Ramazan Guner <rguner@gmail.com>
2023-03-01 14:43:09 +0300

    first commit

---
+++ src/test/java/com/guner/springbootvalidation/SpringbootValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guner.springbootvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
